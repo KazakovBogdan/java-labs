abbb2ebe66c913bceb2e772979b2c31cab946522
User <kazakovb0411@gmail.com>
2024-01-30 17:04:14 +0200

    Lab0: changes output text

--- src/main/java/edu/ntudp/fit/kazakov/lab0/Main.java
+++ src/main/java/edu/ntudp/fit/kazakov/lab0/Main.java
@@ -2,6 +2,6 @@ package edu.ntudp.fit.kazakov.lab0;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        System.out.println("Hello GIT!!");
     }
 }
