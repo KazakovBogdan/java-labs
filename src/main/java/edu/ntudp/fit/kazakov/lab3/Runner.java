4833107736680d0757fcc3cc2cfa4a1c2937b952
User <kazakovb0411@gmail.com>
2024-02-28 11:53:05 +0200

    Lab3: add Runner

---
+++ src/main/java/edu/ntudp/fit/kazakov/lab3/Runner.java
@@ -0,0 +1,9 @@
+package edu.ntudp.fit.kazakov.lab3;
+
+import edu.ntudp.fit.kazakov.lab3.controllers.UniversityCreator;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(UniversityCreator.createTypicalUniversity());
+    }
+}
